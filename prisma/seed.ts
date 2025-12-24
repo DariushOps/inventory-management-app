96a3d1c4f7727f1f927498d1c4fa024f6d24631e
DariushOps <overlord127001@gmail.com>
2025-12-24 12:29:24 +0330

    init

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const demoUserId = "5a37bb34-418e-464e-bef8-22e04d7c942c";
+
+  const productsData = Array.from({ length: 25 }).map((_, i) => ({
+    userId: demoUserId,
+    name: `Product ${i + 1}`,
+    price: parseFloat((Math.random() * 90 + 10).toFixed(2)),
+    quantity: Math.floor(Math.random() * 20),
+    lowStockAt: 5,
+    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+  }));
+
+  await prisma.product.createMany({
+    data: productsData,
+    skipDuplicates: true,
+  });
+
+  console.log("Seed data created successfully!");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
