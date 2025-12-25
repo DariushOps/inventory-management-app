7842c283298c2fd2eb2e2c8c488b155f35ef3991
DariushOps <overlord127001@gmail.com>
2025-12-25 23:21:51 +0330

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { getCurrentUser } from "@/lib/auth";
 import Link from "next/link";
 
 export default async function Home() {
-  const user = await getCurrentUser();
   return (
     <div className="min-h-screen bg-linear-to-br from-purple-50 to-purple-100 flex items-center justify-center">
       <div className="container mx-auto px-4 py-16">
@@ -17,7 +16,7 @@ export default async function Home() {
           </p>
           <div className="flex gap-4 justify-center">
             <Link
-              href={user ? "/dashboard" : "/sign-in"}
+              href="/sign-in"
               className="bg-blue-700 text-white px-8 py-3 rounded-lg font-semibold transition-colors duration-500 hover:bg-blue-800 "
             >
               Sign In
