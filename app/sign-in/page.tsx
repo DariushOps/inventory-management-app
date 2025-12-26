b3c23e4f08e326eaaada344ed368a5915d2b0e38
DariushOps <overlord127001@gmail.com>
2025-12-26 12:39:32 +0330

    update

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,14 @@
-import { SignIn } from "@stackframe/stack";
 import Link from "next/link";
+import { stackServerApp } from "@/stack/server";
+import { redirect } from "next/navigation";
+import { SignIn } from "@stackframe/stack";
 
 export default async function SignInPage() {
+  const user = await stackServerApp.getUser();
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-purple-100">
       <div className="max-w-md w-full space-y-8">
